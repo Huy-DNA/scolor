6d30913df2d67a2af14c34cc7bd4c29290a89dca
Huy-DNA <huydo862003@gmail.com>
2024-10-22 00:04:13 +0700

    fix: remove js logo

--- src/main/scala/scolor/main.scala
+++ src/main/scala/scolor/main.scala
@@ -5,10 +5,6 @@ import scala.scalajs.js.annotation.*
 
 import org.scalajs.dom
 
-// import javascriptLogo from "/javascript.svg"
-@js.native @JSImport("/javascript.svg", JSImport.Default)
-val javascriptLogo: String = js.native
-
 @main
 def Scolor(): Unit =
   dom.document.querySelector("#app").innerHTML = s"""
