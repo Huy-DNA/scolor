29ddbe71b8452d3d957930f7ab739536c13a48a1
Huy-DNA <huydo862003@gmail.com>
2024-11-06 23:05:05 +0700

    feat: pick a random color on each level

--- src/main/scala/scolor/screens/Level.scala
+++ src/main/scala/scolor/screens/Level.scala
@@ -8,8 +8,13 @@ import com.raquo.laminar.api.L.{*, given}
 
 object Level:
   def pageElement(level: Int, maybeLevelBus: EventBus[Option[Int]]): Element =
+    val rand = new scala.util.Random
+    val r = rand.nextInt(256).toHexString.reverse.padTo(2, "0").reverse
+    val g = rand.nextInt(256).toHexString.reverse.padTo(2, "0").reverse
+    val b = rand.nextInt(256).toHexString.reverse.padTo(2, "0").reverse
     div(
       cls := "flex items-center justify-center min-h-[100vh]",
+      backgroundColor := s"#$r$g$b",
       p(
         cls := "text-[30px] text-white drop-shadow-md",
         s"Level $level"
