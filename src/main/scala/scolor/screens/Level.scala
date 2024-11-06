d42fbbfea353f01de0a1f9873a8c24ba6fdb5fb2
Huy-DNA <huydo862003@gmail.com>
2024-11-06 22:52:07 +0700

    feat: add level title to level page

--- src/main/scala/scolor/screens/Level.scala
+++ src/main/scala/scolor/screens/Level.scala
@@ -8,6 +8,12 @@ import com.raquo.laminar.api.L.{*, given}
 
 object Level:
   def pageElement(level: Int, maybeLevelBus: EventBus[Option[Int]]): Element =
-    div()
+    div(
+      cls := "flex items-center justify-center min-h-[100vh]",
+      p(
+        cls := "text-[30px] text-white drop-shadow-md",
+        s"Level $level"
+      ),
+    )
   end pageElement
 end Level
