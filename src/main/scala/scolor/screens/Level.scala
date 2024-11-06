e52235637f9a32953c3b9f043356d882ebac41ac
Huy-DNA <huydo862003@gmail.com>
2024-11-06 23:38:57 +0700

    feat: show color preview for 4s

--- src/main/scala/scolor/screens/Level.scala
+++ src/main/scala/scolor/screens/Level.scala
@@ -7,7 +7,7 @@ import org.scalajs.dom
 import com.raquo.laminar.api.L.{*, given}
 
 object Level:
-  def colorView(level: Int, h: Int, s: Int, l: Int): Element =
+  def colorPreview(level: Int, h: Int, s: Int, l: Int): Element =
     val th = (h + 180) % 360
     val ts = (h + 50) % 100
     val tl = (h + 50) % 100
@@ -21,13 +21,21 @@ object Level:
         s"Level $level",
       ),
     )
-  end colorView
+  end colorPreview
+
+  def colorPick(level: Int, h: Int, s: Int, l: Int, maybeLevelBus: EventBus[Option[Int]]): Element =
+    div()
+  end colorPick
 
   def pageElement(level: Int, maybeLevelBus: EventBus[Option[Int]]): Element =
     val rand = new scala.util.Random
     val h = rand.nextInt(361)
     val s = rand.nextInt(101)
     val l = rand.nextInt(101)
-    colorView(level, h, s, l)
+    val delaySignal = EventStream.delay(4000).map(_ => Some(())).startWith(None)
+    div(child <-- delaySignal.map {
+      case Some(_) => colorPick(level, h, s, l, maybeLevelBus)
+      case None => colorPreview(level, h, s, l)
+    })
   end pageElement
 end Level
