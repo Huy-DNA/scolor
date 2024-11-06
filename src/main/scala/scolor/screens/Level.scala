596d31b677d4470301e5f3a16ab2c4db08aca70d
Huy-DNA <huydo862003@gmail.com>
2024-11-06 23:15:07 +0700

    fix: make the level title color suitable for bg color

--- src/main/scala/scolor/screens/Level.scala
+++ src/main/scala/scolor/screens/Level.scala
@@ -9,14 +9,20 @@ import com.raquo.laminar.api.L.{*, given}
 object Level:
   def pageElement(level: Int, maybeLevelBus: EventBus[Option[Int]]): Element =
     val rand = new scala.util.Random
-    val h = rand.nextInt(360)
+    val h = rand.nextInt(361)
     val s = rand.nextInt(101)
     val l = rand.nextInt(101)
+
+    val th = (h + 180) % 360
+    val ts = (h + 50) % 100
+    val tl = (h + 50) % 100
+
     div(
       cls := "flex items-center justify-center min-h-[100vh]",
       backgroundColor := s"hsl($h, $s%, $l%)",
       p(
-        cls := "text-[30px] text-white drop-shadow-md",
+        cls := "text-[30px] drop-shadow-md",
+        color := s"hsl($th, $ts%, $tl%)",
         s"Level $level"
       ),
     )
