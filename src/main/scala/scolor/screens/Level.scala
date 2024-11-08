50db34a2c9c9949f1204e0262789d24607f82550
Huy-DNA <huydo862003@gmail.com>
2024-11-08 13:41:31 +0700

    feat: basic level screen

--- src/main/scala/scolor/screens/Level.scala
+++ src/main/scala/scolor/screens/Level.scala
@@ -7,15 +7,16 @@ import org.scalajs.dom
 import com.raquo.laminar.api.L.{*, given}
 
 object Level:
-  def previewPane(): Element =
+  def previewPane(h: Int, s: Int, l: Int): Element =
     div(
-      cls := "flex-1 bg-white",
+      backgroundColor := s"hsl($h, $s%, $l%)",
+      cls := "bg-white drop-shadow-lg h-[100%]",
     )
   end previewPane
 
   def pickPane(): Element =
     div(
-      cls := "flex-1 bg-white",
+      cls := "bg-white drop-shadow-lg h-[100%]",
     )
   end pickPane
 
@@ -24,6 +25,7 @@ object Level:
     val h = rand.nextInt(361)
     val s = rand.nextInt(101)
     val l = rand.nextInt(101)
+    val acc = rand.nextInt(35) + 40
     val delaySignal = EventStream.delay(4000).map(_ => Some(())).startWith(None)
     div(
       cls := "min-w-[100vw] min-h-[100vh] bg-gradient-to-r from-cyan-600 to-blue-500 p-16",
@@ -34,9 +36,23 @@ object Level:
           s"Level $level",
         ),
         div(
-          cls := "flex sm:flex-col lg:flex-row gap-16 mt-10 min-h-[75vh]", 
-          Level.previewPane(),
-          Level.pickPane(),
+          cls := "flex sm:flex-col lg:flex-row sm:gap-16 lg:gap-32 mt-6 min-h-[75vh]", 
+          div(
+            cls := "flex-1",
+            p(
+              cls := "text-white text-[15px] mb-2",
+              s"Required accuracy: $acc%",
+            ),
+            Level.previewPane(h, s, l),
+          ),
+          div(
+            cls := "flex-1",
+            p(
+              cls := "text-white text-right text-[15px] mb-2",
+              s"Your accuracy: ??%",
+            ),
+            Level.pickPane(),
+          ),
         ),
       ),
     )
