e25357df388045ff61bbcc0f3f6e3f4b78be3efe
Huy-DNA <huydo862003@gmail.com>
2024-11-20 01:15:02 +0700

    fix: change createTimeoutSignal to createValueTransition

--- src/main/scala/scolor/event.scala
+++ src/main/scala/scolor/event.scala
@@ -3,9 +3,9 @@ package scolor.event
 import com.raquo.laminar.api.L.{*, given}
 
 object Event:
-  def createTimeoutSignal(ms: Int): Signal[Option[Unit]] =
-    EventStream.delay(ms).map(_ => Some(())).startWith(None)
-  end createTimeoutSignal
+  def createValueTransition[T](ms: Int, from: T, to: T): Signal[T] =
+    EventStream.delay(ms).map(_ => to).startWith(from)
+  end createValueTransition
 
   def createPeriodicCounter(ms: Int): EventStream[Int] =
     var counter = 0
