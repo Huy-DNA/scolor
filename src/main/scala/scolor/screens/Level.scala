7168d2bf4023e8f0b2a999a8cef9c139cfc5076d
Huy-DNA <huydo862003@gmail.com>
2024-11-20 00:48:04 +0700

    fix: use Color instead of separate doubles as color components

--- src/main/scala/scolor/screens/Level.scala
+++ src/main/scala/scolor/screens/Level.scala
@@ -6,12 +6,15 @@ import scala.scalajs.js.annotation.*
 import org.scalajs.dom
 import com.raquo.laminar.api.L.{*, given}
 
+import scala.util.Random
+import scolor.color.Color
+
 object Level:
-  def previewPane(h: Int, s: Int, l: Int): Element =
+  def previewPane(previewColor: Color.HSL): Element =
     val delaySignal = EventStream.delay(4000).map(_ => Some(())).startWith(None)
     div(
       backgroundColor <-- delaySignal.map {
-        case None => s"hsl($h, $s%, $l%)"
+        case None => s"hsl(${previewColor.h * 360}, ${previewColor.s * 100}%, ${previewColor.l}%)"
         case Some(_) => "#555555"
       },
       cls := "bg-white drop-shadow-lg h-[30vh] lg:h-[100%] flex items-center justify-center",
@@ -59,11 +62,8 @@ object Level:
   end pickPane
 
   def pageElement(level: Int, maybeLevelBus: EventBus[Option[Int]]): Element =
-    val rand = new scala.util.Random
-    val h = rand.nextInt(301) + 20
-    val s = rand.nextInt(71) + 30
-    val l = rand.nextInt(61) + 30
-    val acc = rand.nextInt(35) + 40
+    val previewColor = Color.HSL(Random.nextDouble(), Random.nextDouble, Random.nextDouble)
+    val acc = Random.nextInt(35) + 40
     val playerAccuracyBus = new EventBus[Option[Float]]
     val playerAccuracySignal = playerAccuracyBus.events.startWith(None)
     val pickedColorBus = new EventBus[Option[String]]
@@ -84,7 +84,7 @@ object Level:
               cls := "text-white text-[15px] mb-2",
               s"Required accuracy: $acc%",
             ),
-            Level.previewPane(h, s, l),
+            Level.previewPane(previewColor.asInstanceOf[Color.HSL]),
           ),
           div(
             cls := "flex-1",
